a04fe03208bb4bb668caabe7419a5cec527c1f59
Pancakse <84547061+Pan-cakse@users.noreply.github.com>
2024-08-08 00:09:39 -0600

    Update PushToAPI.sh

--- scripts/PushToAPI.sh
+++ scripts/PushToAPI.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
 sha256() {
-  sha256sum $1 | awk '{print $1}'
+  sha256sum "$1" | awk '{print $1}'
 }
 
 prop() {
   grep "${1}" gradle.properties | cut -d'=' -f2 | sed 's/\r//'
 }
-echo "$tag"
+
+tag="some_tag_value"
+
 project_id="icecream"
 project_name="icecream"
 mcversion=$(prop mcVersion)
 ctime=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 pre=$(prop preVersion)
-if [ $pre = "true" ]; then
+
+if [ "$pre" = "true" ]; then
   channel="experimental"
 else
   channel="default"
 fi
+
 promoted=false
 
-number=$(git log --oneline master ^`git describe --tags --abbrev=0` | wc -l)
+number=$(git log --oneline master ^$(git describe --tags --abbrev=0) | wc -l)
 changes=$(git log --pretty='%H<<<%s>>>' -"$number" | sed ':a;N;$!ba;s/\n//g')
 jar_name="IceCream-$mcversion.jar"
-jar_sha256=`sha256 $jar_name`
+jar_sha256=$(sha256 "$jar_name")
 
-curl --location --request POST "https://api.icecreammc.xyz/v2/commit/build" --header "Content-Type: application/json" --header "Authentication: Bearer $secret_v2" --data-raw "{\"project_id\":\"$project_id\",\"version\":\"$mcversion\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"tag\":\"$tag\"}"
+curl --location --request POST "https://api.icecreammc.xyz/v2/commit/build" \
+  --header "Content-Type: application/json" \
+  --header "Authentication: Bearer $secret_v2" \
+  --data-raw "{\"project_id\":\"$project_id\",\"version\":\"$mcversion\",\"channel\":\"$channel\",\"changes\":\"$changes\",\"jar_name\":\"$jar_name\",\"sha256\":\"$jar_sha256\",\"tag\":\"$tag\"}"
